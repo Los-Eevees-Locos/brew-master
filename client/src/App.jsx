3451ef59ff3b1d8c3232302b55a5d1d1184f285f
Jason Wong <wongj003@gmail.com>
2024-01-16 16:13:07 -0500

    react boilerplate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
-function App() {
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import GenerateBreweries from './components/GenerateBreweries.jsx';
+
+function App(props) {
   return (
     <div>
       <h1>BEEEEER</h1>
+      <Routes>
+        <Route path="/" element={<GenerateBreweries />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/GenerateBreweries.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const GenerateBreweries = () => {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default GenerateBreweries;
